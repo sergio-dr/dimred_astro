d5495f43af595a9eaea2d68ad8e374912a3d67b5
Sergio <sdiaz@us.es>
2021-12-23 17:35:29 +0100

    First commit (based on the jupyter notebook code)

---
+++ README.md
@@ -0,0 +1,66 @@
+# dimred
+
+Color mapping is always challenging in astrophotography, especially when we count with observations of the same astronomical object in multiple spectral bands. Here I propose a possible method to approach this problem based on a well-known machine learning algorithm borrowed from classic statistics, the Principal Component Analysis, to compress information from the multiband input data into RGB channels while minimizing information loss. This technique could also be applied to narrowband-only "SHO" data to find an encoding that deals with the typical H-alpha overwhelming signal. 
+
+More info about this approach in the [blog page](https://). 
+
+
+## Command line help
+
+```
+usage: dimred.py [-h] [-df DOWNSCALE_FACTOR] [-qm QUANTILE_MIN]
+                 [-qM QUANTILE_MAX] [-cr CHROMA_ROTATION]
+                 [-ce CHROMA_ROTATION_END] [-cf] [-lf] [-e EXPLORE]
+                 input_files output_file
+```
+
+Maps multiple bands or channels (given as a bunch of monochromatic image TIFF files) into a RGB color image. 
+Input data is transformed by a PCA (Principal Components Analysis) algorithm to reduce the dimensionality, 
+effectively compressing the information to three components: the first one (which has the most variance) is 
+interpreted as luminance data, while the second and third components are interpreted as chrominance data. 
+This implementation places this luminance/chrominance data CIELAB color space and then performs its 
+conversion to RGB. 
+
+The chrominance data (a, b components) can be rotated and flipped to generate different color mappings. 
+The --explore N option is useful to explore different color palettes: it generates a NxN mosaic with different
+values for chroma rotation [CHROMA_ROTATION, CHROMA_ROTATION_END) and flipping. Be sure to use it along with 
+a high DOWNSCALE_FACTOR to speed computations. Every image in the mosaic shows the chroma command line 
+parameters needed to generate it. 
+
+In some cases, --luma-flip may be needed if an inverted image is generated.
+
+The input files are assumed to be in non-linear stage (i.e., previously stretched).
+
+Examples:
+* Basic usage:
+```
+  dimred.py *.tif output\pca.tif -cr 30 
+```  
+
+* Exploratory mode, with 5x5 chroma rotation values in the range [30�, 120�], downscaling by 8: 
+```
+  dimred.py *.tif output\pca.tif -e 5 -df 8 -cr 30 -cr 120
+```
+
+```
+positional arguments:
+  input_files           Input files specified using wildcards, e.g. '*.tif'
+  output_file           Output filename
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -df DOWNSCALE_FACTOR, --downscale-factor DOWNSCALE_FACTOR
+                        Integer downscaling factor to speed up computation, as a preview (optional)
+  -qm QUANTILE_MIN, --quantile-min QUANTILE_MIN
+                        Quantile range for chroma scaling, minimum value (optional)
+  -qM QUANTILE_MAX, --quantile-max QUANTILE_MAX
+                        Quantile range for chroma scaling, maximum value (optional)
+  -cr CHROMA_ROTATION, --chroma-rotation CHROMA_ROTATION
+                        Angle (in degrees) for specifying chroma components rotation; will be interpreted as initial angle in --explore mode (optional)
+  -ce CHROMA_ROTATION_END, --chroma-rotation-end CHROMA_ROTATION_END
+                        End angle (in degrees) for in --explore mode; by default, 360�+CHROMA_ROTATION (optional)
+  -cf, --chroma-flip
+  -lf, --luma-flip
+  -e EXPLORE, --explore EXPLORE
+                        Exploratory mode: given an integer N, generates a NxN mosaic with the --chroma-angle range specified (optional)
+```
---
+++ dimred.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+
+# %%
+import argparse
+import os
+from glob import iglob 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from skimage.io import imread, imsave
+from skimage.transform import downscale_local_mean
+from skimage.color import lab2rgb # TODO:, ydbdr2rgb, ...
+from skimage.util import img_as_uint
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler, RobustScaler
+
+np.set_printoptions(precision=4)
+
+
+n_components = 3
+luma_factor = 100
+chroma_factor = 90
+
+
+DESC="""
+Maps multiple bands or channels (given as a bunch of monochromatic image TIFF files) into a RGB color image. 
+Input data is transformed by a PCA (Principal Components Analysis) algorithm to reduce the dimensionality, 
+effectively compressing the information to three components: the first one (which has the most variance) is 
+interpreted as luminance data, while the second and third components are interpreted as chrominance data. 
+This implementation places this luminance/chrominance data CIELAB color space and then performs its 
+conversion to RGB. 
+
+The chrominance data (a, b components) can be rotated and flipped to generate different color mappings. 
+The --explore N option is useful to explore different color palettes: it generates a NxN mosaic with different
+values for chroma rotation [CHROMA_ROTATION, CHROMA_ROTATION_END) and flipping. Be sure to use it along with 
+a high DOWNSCALE_FACTOR to speed computations. Every image in the mosaic shows the chroma command line 
+parameters needed to generate it. 
+
+In some cases, --luma-flip may be needed if an inverted image is generated.
+
+The input files are assumed to be in non-linear stage (i.e., previously stretched).
+
+Examples:
+* Basic usage:
+```
+  dimred.py *.tif output\pca.tif -cr 30 
+```  
+
+* Exploratory mode, with 5x5 chroma rotation values in the range [30º, 120º], downscaling by 8: 
+```
+  dimred.py *.tif output\pca.tif -e 5 -df 8 -cr 30 -cr 120
+```
+
+"""
+
+DEBUG_CMDLINE = None
+#DEBUG_CMDLINE = "*.tif pca.tif -df 8 -e 5".split(" ") 
+
+config_defaults = {
+    'downscale_factor': 1,
+    'quantile_min': 0.01,
+    'quantile_max': 99.99,
+    'chroma_rotation': 0.0,
+    'explore': 0,
+}
+
+parser = argparse.ArgumentParser(description=DESC, formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("input_files", 
+                    help="Input files specified using wildcards, e.g. '*.tif'")
+parser.add_argument("output_file", 
+                    help="Output filename") 
+parser.add_argument("-df", "--downscale-factor", type=int, default=config_defaults['downscale_factor'],
+                    help="Integer downscaling factor to speed up computation, as a preview (optional)")
+parser.add_argument("-qm", "--quantile-min", type=float, default=config_defaults['quantile_min'],
+                    help="Quantile range for chroma scaling, minimum value (optional)")
+parser.add_argument("-qM", "--quantile-max", type=float, default=config_defaults['quantile_max'],
+                    help="Quantile range for chroma scaling, maximum value (optional)")
+parser.add_argument("-cr", "--chroma-rotation", type=float, default=config_defaults['chroma_rotation'],
+                    help="Angle (in degrees) for specifying chroma components rotation; will be interpreted as initial angle in --explore mode (optional)")
+parser.add_argument("-ce", "--chroma-rotation-end", type=float, 
+                    help="End angle (in degrees) for in --explore mode; by default, 360º+CHROMA_ROTATION (optional)")
+parser.add_argument("-cf", "--chroma-flip", action='store_true',
+                    help="")
+parser.add_argument("-lf", "--luma-flip", action='store_true',
+                    help="")
+parser.add_argument("-e", "--explore", type=int, default=config_defaults['explore'],
+                    help="Exploratory mode: given an integer N, generates a NxN mosaic with the --chroma-angle range specified (optional)")
+
+
+args = parser.parse_args(DEBUG_CMDLINE)
+config = vars(args)
+
+
+
+# __/ Prepare output directory \__________
+output_path = os.path.dirname(os.path.join('.', config['output_file']))
+os.makedirs(output_path, exist_ok=True)
+
+
+# __/ Open input files and generate the stack \__________
+path_tpl = os.path.join('.', config['input_files'])
+images = []
+print("Input images:")
+for filename in iglob(path_tpl):
+    im = imread(filename)
+    print(filename, im.dtype, im.shape)        
+    im = im.astype(np.float32) / (2**16 - 1)    
+    if config['downscale_factor'] > 1:
+        im = downscale_local_mean(im, (config['downscale_factor'], config['downscale_factor']))
+    images.append( im )
+print()
+
+data = np.stack(images, axis=-1)
+del im, images
+
+# %%
+
+# __/ Apply the PCA transformation \__________
+h, w, channels = data.shape
+data = data.reshape((-1, channels))
+
+pca = PCA(n_components=n_components)
+embedding = pca.fit_transform(data)
+del data
+
+evr = pca.explained_variance_ratio_
+print("Explained variance ratio: ", evr, "\n")
+
+
+# %%
+
+# __/ Scale the principal components \__________
+luma_scaler = MinMaxScaler()
+
+quantile_range = (config['quantile_min'], config['quantile_max'])
+chroma_scaler = RobustScaler(quantile_range=quantile_range) 
+
+luma = luma_factor * luma_scaler.fit_transform(embedding[:, [0]])
+chroma = chroma_factor * chroma_scaler.fit_transform(embedding[:, [1, 2]])
+
+lum_chr = np.concatenate([luma, chroma], axis=-1)
+
+print("Lab channels range:")
+lab_ch_names = "Lab"
+for ch in range(n_components):
+    print(f"{lab_ch_names[ch]} : {lum_chr[:, ch].min():.2f} .. {lum_chr[:, ch].max():.2f}")
+print()
+
+# %%
+
+# __/ Luma/Chroma transformations \__________
+
+def luma_flipping(lum_chr):
+    return lum_chr * [-1, 1, 1]
+
+def chroma_flipping(lum_chr):
+    # just need to flip first chroma component; 
+    # flipping the second is equivalent to flipping the first and rotating -180º;
+    # flipping both and rotating -180º is the same as no flipping. 
+    return lum_chr * [1, -1, 1] 
+
+def chroma_rotation(lum_chr, degs):
+    rads = degs * (np.pi/180)
+    cos_a = np.cos(rads)
+    sin_a = np.sin(rads)
+
+    ch_mapping = np.array([ 
+        [1, 0, 0],
+        [0, cos_a, sin_a],
+        [0, -sin_a, cos_a]
+    ]).T
+
+    return lum_chr @ ch_mapping
+
+
+# __/ Apply luma/chroma transformations and write output \__________
+
+if config['luma_flip']:
+    lum_chr = luma_flipping(lum_chr)
+
+if config['explore'] == 0:
+    print("Generating output image...")
+    if config['chroma_flip']:
+        lum_chr = chroma_flipping(lum_chr)    
+    lum_chr = chroma_rotation(lum_chr, config['chroma_rotation'])
+
+    for ch in range(n_components):
+        print( lum_chr[:, ch].min(), lum_chr[:, ch].max() )
+
+    lum_chr = lum_chr.reshape((h, w, 3))
+    rgb = lab2rgb(lum_chr) 
+    imsave(config['output_file'], img_as_uint(rgb))
+else:
+    print("Generating mosaic...")
+    N = config['explore']
+    M = N*N
+    cols, rows = N, 2*N
+    hue_i = config['chroma_rotation']
+    hue_f = config['chroma_rotation_end'] if config['chroma_rotation_end'] is not None else config['chroma_rotation']+360 
+
+    ar = w/h
+    S = 4
+    fig, axs = plt.subplots(rows, cols, figsize=(ar*S*cols, S*rows), facecolor='dimgray')
+    axs = axs.ravel()
+    for i, degs in enumerate(np.linspace(hue_i, hue_f, M, endpoint=False)):
+        lum_chr_degs = chroma_rotation(lum_chr, degs).reshape((h, w, 3))
+        rgb_hue = lab2rgb(lum_chr_degs)
+        axs[i].imshow(rgb_hue)
+        axs[i].set_axis_off()
+        axs[i].text(0, 16, f"-cr={degs:.1f}", c='white', fontsize=16)
+    lum_chr = chroma_flipping(lum_chr)  
+    for i, degs in enumerate(np.linspace(hue_i, hue_f, M, endpoint=False)):
+        lum_chr_degs = chroma_rotation(lum_chr, degs).reshape((h, w, 3))
+        rgb_hue = lab2rgb(lum_chr_degs)
+        axs[i+M].imshow(rgb_hue)
+        axs[i+M].set_axis_off()
+        axs[i+M].text(0, 16, f"-cf -cr={degs:.1f}", c='white', fontsize=16)
+    fig.tight_layout()
+    fig.savefig(config['output_file'])
+
+print("done.\n")
+# %%
